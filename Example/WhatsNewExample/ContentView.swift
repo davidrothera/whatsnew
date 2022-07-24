4fea21d3114868b874d8eb23af3659e261f0eab9
David Rothera <david.rothera@gmail.com>
2022-07-24 21:52:29 +0100

    Fix example app

--- Example/WhatsNewExample/ContentView.swift
+++ Example/WhatsNewExample/ContentView.swift
@@ -17,10 +17,10 @@ struct ContentView: View {
         Text("Hello, world!")
             .padding()
             .whatsNew(whatsNew: .init(items: [
-                .init(title: "Test", body: "Something long which can be written so that we make sure that the text spills over into the next line. Once t does then it gives us a good chance to look at layout issues.", colorName: "blue", iconName: "scribble.variable"),
-                .init(title: "Test", body: "Something", colorName: "yellow", iconName: "trash.fill"),
-                .init(title: "Test", body: "Something", shownOnVersion: "1.0", colorName: "mint", iconName: "tray.fill"),
-                .init(title: "Test", body: "Something", shownOnVersion: "123", colorName: "indigo", iconName: "moon.fill"),
+                .init(id: "first", title: "Test", body: "Something long which can be written so that we make sure that the text spills over into the next line. Once t does then it gives us a good chance to look at layout issues.", colorName: "blue", iconName: "scribble.variable"),
+                .init(id: "second", title: "Test", body: "Something", colorName: "yellow", iconName: "trash.fill"),
+                .init(id: "third", title: "Test", body: "Something", shownOnVersion: "1.0", colorName: "mint", iconName: "tray.fill"),
+                .init(id: "fourth", title: "Test", body: "Something", shownOnVersion: "123", colorName: "indigo", iconName: "moon.fill"),
             ], forceShow: true))
     }
 }
